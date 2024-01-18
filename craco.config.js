83c22c88d427268f10da78f5ca6472557fdc7bdc
haggai yaniv <yhaggai@gmail.com>
2024-01-18 16:36:45 +0200

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,6 @@
 module.exports = require('@kentcdodds/react-workshop-app/craco.config')
+var urlParams = new URLSearchParams(window.location.search);
+var username = urlParams.get('username');
+
+var unsafe_div = window.document.getElementById("vulnerable-div");
+unsafe_div.innerHTML = "Hello to you " + username;
